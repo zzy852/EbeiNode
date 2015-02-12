9a6052baf5f80385ded3dd69384fe2ede9ccab21
zzy852 <zyb8523790@163.com>
2015-02-12 18:29:25 +0800

    add

---
+++ Server/ebei/routes/logout.js
@@ -0,0 +1,16 @@
+/**
+ * logout.js
+ * @zzy
+ * @date    2015-02-06 17:38:00
+ * @version 1.0.0
+ */
+
+var express = require('express'),
+     router = express.Router();
+
+router.get('/', function(req, res) {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+module.exports = router;
---
+++ Server/ebei/routes/usercenter.js
@@ -0,0 +1,79 @@
+var express = require('express'),
+  router = express.Router(),
+  formidable = require('formidable'),
+  fs = require('fs'),
+  AVATAR_UPLOAD_FOLDER = '/upload/';
+
+router.get('/', function(req, res) {
+  if (req.cookies.islogin) {
+    console.log('cookies:' + req.cookies.islogin);
+    req.session.username = req.cookies.islogin;
+  }
+  if (req.session.username) {
+    console.log('session:' + req.session.username);
+    res.locals.username = req.session.username;
+  } else {
+    res.redirect('/login');
+    return;
+  }
+  res.render('usercenter', {
+    title: '用户中心'
+  });
+});
+
+router.post('/', function(req, res) {
+
+  var form = new formidable.IncomingForm(); //创建上传表单
+  form.encoding = 'utf-8'; //设置编辑
+  form.uploadDir = 'public' + AVATAR_UPLOAD_FOLDER; //设置上传目录
+  form.keepExtensions = true; //保留后缀
+  form.maxFieldsSize = 2 * 1024 * 1024; //文件大小
+
+  form.parse(req, function(err, fields, files) {
+
+    if (err) {
+      res.locals.error = err;
+      res.render('index', {
+        title: TITLE
+      });
+      return;
+    }
+
+    var extName = ''; //后缀名
+    switch (files.qqfile.type) {
+      case 'image/pjpeg':
+        extName = 'jpg';
+        break;
+      case 'image/jpeg':
+        extName = 'jpg';
+        break;
+      case 'image/png':
+        extName = 'png';
+        break;
+      case 'image/x-png':
+        extName = 'png';
+        break;
+    }
+
+    if (extName.length === 0) {
+      res.locals.error = '只支持png和jpg格式图片';
+        res.render('usercenter', {
+            title: '用户中心'
+        });
+      return;
+    }
+
+    var avatarName = Math.random() + '.' + extName;
+    var newPath = form.uploadDir + avatarName;
+
+    console.log(newPath);
+    fs.renameSync(files.qqfile.path, newPath); //重命名
+  });
+
+  res.locals.success = '上传成功';
+    res.render('usercenter', {
+        title: '用户中心'
+    });
+});
+
+module.exports = router;
